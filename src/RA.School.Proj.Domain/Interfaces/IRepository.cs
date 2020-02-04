cf120a1ff9e4846f03fbd2258e897d1c0d7a3979
ricardo albino <ricardoalbino@outlook.com>
2020-02-03 21:31:24 -0300

    Alterações

---
+++ src/RA.School.Proj.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RA.School.Proj.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity :  class
+    {
+        Task adicionar(TEntity  entity);
+        Task Atualizar(TEntity entity);
+        Task Remover(TEntity entity);
+        Task<List<TEntity>> ObterTodos();
+        Task<TEntity> ObterPorId(Guid id);
+        Task<IEnumerable<TEntity>> buscar(Expression<Func<TEntity, bool>> predicate);
+
+    }
+}
